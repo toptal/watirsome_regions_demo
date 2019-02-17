8c988f43566e524ebd835cdcdd5dab84633565aa
Bartek Wilczek <bartek.wilczek@toptal.com>
2019-02-17 07:36:18 +0100

    Add expectations for region_element

--- spec/todo_lists_spec.rb
+++ spec/todo_lists_spec.rb
@@ -12,6 +12,12 @@ RSpec.describe TodoListsPage do
       expect(page.work_list.items.first.name).to eq 'Review the PR-1234'
 
       expect(page.todo_lists['Groceries'].items[0].name).to eq 'Bread'
+
+      # region_element
+      expect(page.home_list.region_element.id).to eq 'todos_home'   # as declared in `has_one :home_list`
+      expect(page.work_list.region_element.id).to eq 'todos_work'   # as declared in `has_one :work_list`
+      expect(page.todo_lists.region_element).to be_a Watir::Browser # no 'in:' given to `has_many :todo_lists`
+      expect(page.region_element).to be_a Watir::Browser            # Browser is the region_element for Pages
     end
   end
 end
